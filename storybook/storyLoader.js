85a474bdb4423ec669fb7ee7741ed5553d4f5be0
Christopher Severns <chris@g2i.co>
2018-11-29 13:02:29 -0500

    configure storybook

---
+++ storybook/storyLoader.js
@@ -0,0 +1,20 @@
+
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/components/Button/button.stories');
+  
+}
+
+const stories = [
+  '../src/components/Button/button.stories',
+  
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
