4c537d67029f5ea60762bcf57612686d2e71bd93
Christopher Severns <chris@g2i.co>
2018-12-01 11:19:55 -0500

    Mock out native TextInput

--- src/components/TextInput/textInput.stories.js
+++ src/components/TextInput/textInput.stories.js
@@ -4,20 +4,33 @@ import { storiesOf } from '@storybook/react-native';
 
 storiesOf("TextInput", module)
 	.add("with value", () => (
-		<TextInput value="input goes here" />
+		<TextInput value="input goes here" onChange={() => {}} />
 	))
 	.add("with placeholder", () => (
-		<TextInput placeholder="placeholder" />
+		<TextInput placeholder="placeholder" onChange={() => {}} />
 	))
 	.add("with password", () => (
-		<TextInput password={true} value="password" />
+		<TextInput password={true} value="password" onChange={() => {}} />
 	))
 	.add("with autoFocus", () => (
-		<TextInput autoFocus={true} label="Focus" value="Im focused!" />
+		<TextInput
+			autoFocus={true}
+			label="Focus"
+			value="Im focused!"
+			onChange={() => {}}
+		/>
 	))
 	.add("with label", () => (
-		<TextInput label="Username" placeholder="Enter your username" />
+		<TextInput
+			label="Username"
+			placeholder="Enter your username"
+			onChange={() => {}}
+		/>
 	))
 	.add("with isError", () => (
-		<TextInput isError={true} label="Password" />
+		<TextInput 
+			isError={true}
+			label="Password"
+			onChange={() => {}}
+		/>
 	))
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,16 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+jest.mock('TextInput', () => {
+  const RealComponent = require.requireActual('TextInput');
+  const React = require('React');
+
+  class TextInput extends React.Component {
+    render() {
+      return React.createElement('TextInput', {...this.props, autoFocus: false}, this.props.children);
+    }
+  }
+  TextInput.propTypes = RealComponent.propTypes;
+  return TextInput;
+});
