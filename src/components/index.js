dd970df18575c5258887af7f123f954bf084a069
Christopher Severns <chris@g2i.co>
2018-12-19 17:38:29 -0500

    Add Form to exports for stories

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import EmailInput from "./EmailInput";
 import Text from "./Text";
 import Subtitle from "./Subtitle";
 import Tab from "./Tab";
+import Form from "./Form";
 
-export { Button, TextInput, EmailInput, Text, Subtitle, Tab };
+export { Button, TextInput, EmailInput, Text, Subtitle, Tab, Form };
