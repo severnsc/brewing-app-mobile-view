93bbc9bf0c934d87d629d7855320e7ec61563ac1
Christopher Severns <chris@g2i.co>
2018-12-31 11:46:30 -0500

    Use knobs for Clock stories

--- src/components/Clock/clock.stories.js
+++ src/components/Clock/clock.stories.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Clock from ".";
 import { storiesOf } from "@storybook/react-native";
+import { withKnobs, number, select } from "@storybook/addon-knobs";
 
 storiesOf("Clock", module)
-	.add("with seconds only", () => <Clock ms={1000} />)
-	.add("with minutes only", () => <Clock ms={60000} />)
-	.add("with hours only", () => <Clock ms={3600000} />)
-	.add("with minutes and seconds", () => <Clock ms={61000} />)
-	.add("with hours, minutes and seconds", () => <Clock ms={3661000} />)
-	.add("with sm", () => <Clock ms={1000} size="sm" />)
-	.add("with md", () => <Clock ms={1000} size="md" />)
-	.add("with lg", () => <Clock ms={1000} size="lg" />);
+	.addDecorator(withKnobs)
+	.add("default", () => (
+		<Clock
+			ms={number("Milliseconds", 1000)}
+			size={select("Size", ["sm", "md", "lg"], "md")}
+		/>
+	));
