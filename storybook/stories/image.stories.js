5d33f0ed06f04a261ebdf8461a7659f8c0bb09f0
Christopher Severns <chris@g2i.co>
2018-12-31 12:20:22 -0500

    Add knobs to Image

--- storybook/stories/image.stories.js
+++ storybook/stories/image.stories.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Image, View } from "react-native";
 import { storiesOf } from "@storybook/react-native";
+import { withKnobs, number } from "@storybook/addon-knobs";
 
 storiesOf("Image", module)
-	.add("default", () => <Image source={require("../../assets/icon.png")} />)
-	.add("with blurRadius", () => (
-		<Image source={require("../../assets/icon.png")} blurRadius={20} />
+	.addDecorator(withKnobs)
+	.add("default", () => (
+		<Image
+			source={require("../../assets/icon.png")}
+			blurRadius={number("Blur radius", 20, {}, "Image")}
+		/>
 	));
