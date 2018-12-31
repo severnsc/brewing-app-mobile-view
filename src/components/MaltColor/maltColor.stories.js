9920019429b9ac8d68db5ee62a16238845e1aff1
Christopher Severns <chris@g2i.co>
2018-12-31 12:04:05 -0500

    Add groupId to MaltColor

--- src/components/MaltColor/maltColor.stories.js
+++ src/components/MaltColor/maltColor.stories.js
@@ -5,16 +5,22 @@ import { withKnobs, number } from "@storybook/addon-knobs";
 
 const label = "Color in L";
 const defaultValue = 1;
+groupId = "MaltColor";
 
 storiesOf("MaltColor", module)
 	.addDecorator(withKnobs)
-	.add("default", () => <MaltColor value={number(label, defaultValue)} />)
+	.add("default", () => (
+		<MaltColor value={number(label, defaultValue, {}, groupId)} />
+	))
 	.add("with displayValue false", () => (
-		<MaltColor value={number(label, defaultValue)} displayValue={false} />
+		<MaltColor
+			value={number(label, defaultValue, {}, groupId)}
+			displayValue={false}
+		/>
 	))
 	.add("with styles", () => (
 		<MaltColor
-			value={number(label, defaultValue)}
+			value={number(label, defaultValue, {}, groupId)}
 			displayValue={false}
 			containerStyle={{ width: 50, height: 50 }}
 			colorStyle={{ width: 50, height: 50, borderRadius: 100 }}
