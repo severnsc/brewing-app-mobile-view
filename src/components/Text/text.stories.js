4867474ce6c8cd2e028ab2aadf2e18040394ff74
Christopher Severns <chris@g2i.co>
2018-12-31 12:11:12 -0500

    Add knobs to Text stories

--- src/components/Text/text.stories.js
+++ src/components/Text/text.stories.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Text from ".";
 import { storiesOf } from "@storybook/react-native";
+import {
+	withKnobs,
+	text,
+	boolean,
+	color,
+	object
+} from "@storybook/addon-knobs";
 
-storiesOf("Text", module)
-	.add("default", () => <Text value="some text" />)
-	.add("with bold", () => <Text value="bold text" bold={true} />)
-	.add("with color", () => <Text value="I'm red" color="red" />)
-	.add("with success", () => <Text value="Success!" success={true} />)
-	.add("with danger", () => <Text value="Error!" danger={true} />)
-	.add("with style", () => <Text value="larger" style={{ fontSize: 32 }} />);
+const groupId = "Text";
+storiesOf("Text", module).add("default", () => (
+	<Text
+		value={text("Value", "Text", groupId)}
+		bold={boolean("Bold", false, groupId)}
+		color={color("Color", "red", groupId)}
+		success={boolean("Success", false, groupId)}
+		danger={boolean("Danger", false, groupId)}
+		style={object("Style", {}, groupId)}
+	/>
+));
