a4a58a6dc02574b3b3dbaf3e4aec4a1e701c10a5
Christopher Severns <chris@g2i.co>
2018-12-31 12:08:55 -0500

    Add knobs to Tab stories

--- src/components/Tab/tab.stories.js
+++ src/components/Tab/tab.stories.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Tab from ".";
 import { storiesOf } from "@storybook/react-native";
+import { withKnobs, boolean, text } from "@storybook/addon-knobs";
+
+const groupId = "Tab";
 const onPress = () => {};
-storiesOf("Tab", module)
-	.add("inactive", () => <Tab value="Tab" onPress={onPress} />)
-	.add("active", () => <Tab active={true} value="Tab" onPress={onPress} />);
+storiesOf("Tab", module).add("default", () => (
+	<Tab
+		active={boolean("Active", false, groupId)}
+		value={text("Value", "Tab", groupId)}
+		onPress={onPress}
+	/>
+));
