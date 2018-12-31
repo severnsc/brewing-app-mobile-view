9489692801c09e29b70feb416af544dcbdfd0783
Christopher Severns <chris@g2i.co>
2018-12-31 12:02:39 -0500

    Use knobs on form stories

--- src/components/Form/form.stories.js
+++ src/components/Form/form.stories.js
@@ -2,61 +2,35 @@ import React from "react";
 import Form from ".";
 import { TextInput, EmailInput, Button } from "..";
 import { storiesOf } from "@storybook/react-native";
+import { withKnobs, text, object } from "@storybook/addon-knobs";
 
 const onChange = (onPress = () => {});
 
-storiesOf("Form", module)
-	.add("with children", () => (
-		<Form onSubmit={() => {}}>
-			{(values, onChange, onSubmit) => {
-				const [first, second, ...rest] = values;
-				return (
-					<React.Fragment>
-						<TextInput
-							label="Text"
-							value={first && first.value}
-							placeholder="Enter text here"
-							onChange={onChange}
-						/>
-						<EmailInput
-							label="Email"
-							value={second && second.value}
-							placeholder="Enter email here"
-							onChange={onChange}
-						/>
-						<Button success={true} value="submit" onPress={onSubmit} />
-					</React.Fragment>
-				);
-			}}
-		</Form>
-	))
-	.add("with style", () => (
-		<Form
-			onSubmit={() => {}}
-			style={{
-				borderWidth: 1,
-				borderColor: "black"
-			}}
-		>
-			{(values, onChange, onSubmit) => {
-				const [first, second, ...rest] = values;
-				return (
-					<React.Fragment>
-						<TextInput
-							label="Text"
-							value={first && first.value}
-							placeholder="Enter text here"
-							onChange={onChange}
-						/>
-						<EmailInput
-							label="Email"
-							value={second && second.value}
-							placeholder="Enter email here"
-							onChange={onChange}
-						/>
-						<Button onPress={onSubmit} value="submit" success={true} />
-					</React.Fragment>
-				);
-			}}
-		</Form>
-	));
+storiesOf("Form", module).add("with children", () => (
+	<Form onSubmit={() => {}} style={object("Style", {}, "Form")}>
+		{(values, onChange, onSubmit) => {
+			const [first, second, ...rest] = values;
+			return (
+				<React.Fragment>
+					<TextInput
+						label={text("Label", "Text", "Form")}
+						value={first && first.value}
+						placeholder={text("Placeholder", "enter text", "Form")}
+						onChange={onChange}
+					/>
+					<EmailInput
+						label={text("Label 2", "Email", "Form")}
+						value={second && second.value}
+						placeholder={text("Placeholder 2", "enter email", "Form")}
+						onChange={onChange}
+					/>
+					<Button
+						success={true}
+						value={text("Button value", "submit", "Form")}
+						onPress={onSubmit}
+					/>
+				</React.Fragment>
+			);
+		}}
+	</Form>
+));
