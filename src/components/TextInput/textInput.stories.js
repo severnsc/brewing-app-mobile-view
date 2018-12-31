f05b4cb3c9a50e109d6c82374828e66c89b47823
Christopher Severns <chris@g2i.co>
2018-12-31 12:13:54 -0500

    Add knobs to TextInput stories

--- src/components/TextInput/textInput.stories.js
+++ src/components/TextInput/textInput.stories.js
@@ -1,32 +1,18 @@
 import React from "react";
 import TextInput from ".";
 import { storiesOf } from "@storybook/react-native";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs";
 
-storiesOf("TextInput", module)
-  .add("with value", () => (
-    <TextInput value="input goes here" onChange={() => {}} />
-  ))
-  .add("with placeholder", () => (
-    <TextInput placeholder="placeholder" onChange={() => {}} />
-  ))
-  .add("with password", () => (
-    <TextInput password={true} value="password" onChange={() => {}} />
-  ))
-  .add("with autoFocus", () => (
-    <TextInput
-      autoFocus={true}
-      label="Focus"
-      value="Im focused!"
-      onChange={() => {}}
-    />
-  ))
-  .add("with label", () => (
-    <TextInput
-      label="Username"
-      placeholder="Enter your username"
-      onChange={() => {}}
-    />
-  ))
-  .add("with isError", () => (
-    <TextInput isError={true} label="Password" onChange={() => {}} />
-  ));
+const groupId = "TextInput";
+
+storiesOf("TextInput", module).add("with value", () => (
+  <TextInput
+    label={text("Label", "Username", groupId)}
+    value={text("Value", "text goes here", groupId)}
+    placeholder={text("Placeholder", "", groupId)}
+    password={boolean("Password", false, groupId)}
+    autoFocus={boolean("Auto focus", false, groupId)}
+    isError={boolean("Error", false, groupId)}
+    onChange={() => {}}
+  />
+));
