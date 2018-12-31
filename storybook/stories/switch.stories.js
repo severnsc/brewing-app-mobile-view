7d76e7bed654e8d4b43737b87fdcf241b427d433
Christopher Severns <chris@g2i.co>
2018-12-31 12:21:39 -0500

    Add groupId to Switch

--- storybook/stories/switch.stories.js
+++ storybook/stories/switch.stories.js
@@ -24,15 +24,20 @@ class Switcher extends React.Component {
 	}
 }
 
+const groupId = "Switch";
 storiesOf("Switch", module)
 	.addDecorator(withKnobs)
-	.add("default", () => <Switcher disabled={boolean("Diabled", false)} />)
+	.add("default", () => (
+		<Switcher disabled={boolean("Diabled", false, groupId)} />
+	))
 	.add("with ThumbColor", () => (
-		<Switcher thumbColor={color("Thumb Color", "#000")} />
+		<Switcher thumbColor={color("Thumb Color", "#000", groupId)} />
 	))
 	.add("with ios_backgroundColor", () => (
-		<Switcher ios_backgroundColor={color("iOS Background Color", "#000")} />
+		<Switcher
+			ios_backgroundColor={color("iOS Background Color", "#000", groupId)}
+		/>
 	))
 	.add("with track color", () => (
-		<Switcher trackColor={color("Track Color", "#000")} />
+		<Switcher trackColor={color("Track Color", "#000", groupId)} />
 	));
