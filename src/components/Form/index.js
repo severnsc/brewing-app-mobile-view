25bb574a4a0c63977f49d2e3c78e61cc9da7f714
Christopher Severns <chris@g2i.co>
2019-01-01 14:50:15 -0500

    Remove unecessary imports

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { TextInput, EmailInput, Button } from "..";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import PropTypes from "prop-types";
 
 class Form extends React.Component {
