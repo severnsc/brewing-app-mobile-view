f8818ad7fcccc9c24051b2bfb241ef00167a692d
Christopher Severns <chris@g2i.co>
2019-01-11 12:06:58 -0500

    Tests for home screen

--- e2e/screens/Home/home.spec.js
+++ e2e/screens/Home/home.spec.js
@@ -7,4 +7,21 @@ describe("Home", () => {
 	it("should display text Home", async () => {
 		await expect(element(by.id("HomeText"))).toBeVisible();
 	});
+	it("should display image", async () => {
+		await expect(element(by.id("Hero"))).toBeVisible();
+	});
+	it("should have a button to create account", async () => {
+		await expect(element(by.id("ToCreateAccount"))).toBeVisible();
+	});
+	it("should have a button to login", async () => {
+		await expect(element(by.id("ToLogin"))).toBeVisible();
+	});
+	describe("interactions", () => {
+		describe("navigating to create account", () => {
+			it("should display create account text after navigating", async () => {
+				await element(by.id("ToCreateAccount")).tap();
+				await expect(element(by.label("Sign Up"))).toBeVisible;
+			});
+		});
+	});
 });
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -7,20 +7,22 @@ import { white, CREATE_ACCOUNT, LOGIN } from "../../constants";
 const Home = ({ navigation: { navigate } }) => (
 	<GradientView>
 		<View style={styles.container}>
-			<Title value="Brewing App" color={white} />
-			<Image source={require("../../../assets/icon.png")} />
+			<Title value="Brewing App" color={white} testID="HomeText" />
+			<Image testID="Hero" source={require("../../../assets/icon.png")} />
 			<View style={styles.buttonRow}>
 				<Button
 					onPress={() => navigate(CREATE_ACCOUNT)}
 					value="Create an account"
 					style={styles.button}
 					secondary
+					testID="ToCreateAccount"
 				/>
 				<Button
 					onPress={() => navigate(LOGIN)}
 					value="Login"
 					style={styles.button}
 					secondary
+					testID="ToLogin"
 				/>
 			</View>
 		</View>
