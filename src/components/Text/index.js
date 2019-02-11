60c9c6427966e3daad11b964b55d3ae72147bb71
Chris Severns <severnsc@gmail.com>
2019-02-11 09:08:40 -0500

    add accessibilityLabel to Text

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -4,27 +4,32 @@ import { Text as RNText } from "react-native";
 import styles from "./styles";
 
 const Text = props => {
-	const propNames = Object.keys(props);
-	const style = propNames.reduce(
-		(obj, propName) => ({ ...obj, ...styles[propName] }),
-		{ color: props.color, ...props.style }
-	);
-	return (
-		<RNText testID={props.testID} style={style} onPress={props.onPress}>
-			{props.value}
-		</RNText>
-	);
+  const propNames = Object.keys(props);
+  const style = propNames.reduce(
+    (obj, propName) => ({ ...obj, ...styles[propName] }),
+    { color: props.color, ...props.style }
+  );
+  return (
+    <RNText
+      testID={props.testID}
+      style={style}
+      onPress={props.onPress}
+      accessibilityLabel={props.value}
+    >
+      {props.value}
+    </RNText>
+  );
 };
 
 Text.propTypes = {
-	value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-	bold: PropTypes.bool,
-	color: PropTypes.string,
-	success: PropTypes.bool,
-	danger: PropTypes.bool,
-	onPress: PropTypes.func,
-	style: PropTypes.object,
-	testID: PropTypes.string
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  bold: PropTypes.bool,
+  color: PropTypes.string,
+  success: PropTypes.bool,
+  danger: PropTypes.bool,
+  onPress: PropTypes.func,
+  style: PropTypes.object,
+  testID: PropTypes.string
 };
 
 export default Text;
