a00f8ee21b637966d76440dee5263e72bc11dced
Chris Severns <severnsc@gmail.com>
2019-02-16 11:32:03 -0500

    Add e2e test for createAccount failure

--- e2e/screens/CreateAccount/createAccount.spec.js
+++ e2e/screens/CreateAccount/createAccount.spec.js
@@ -8,11 +8,15 @@ describe("Create account", () => {
   describe("interactions", () => {
     describe("error path", () => {
       describe("only entering username", () => {
-        it("should render an error message", async () => {
+        it("should render an error message for email, password & confirm password", async () => {
           await element(by.id("signupUsername")).tap();
           await element(by.id("signupUsername")).typeText("newuser");
           await element(by.id("signupFormButton")).tap();
-          await expect(element(by.id("signupFormError"))).toBeVisible();
+          await expect(element(by.id("emailInputError"))).toBeVisible();
+          await expect(element(by.id("passwordInputError"))).toBeVisible();
+          await expect(
+            element(by.id("confirmPasswordInputError"))
+          ).toBeVisible();
         });
       });
     });
