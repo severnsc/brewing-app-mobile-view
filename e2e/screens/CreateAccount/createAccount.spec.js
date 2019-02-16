9b218b8c90860f685a0a4015ee8d0babe32491fa
Chris Severns <severnsc@gmail.com>
2019-02-16 10:15:58 -0500

    Simplify createAccount e2e tests

--- e2e/screens/CreateAccount/createAccount.spec.js
+++ e2e/screens/CreateAccount/createAccount.spec.js
@@ -5,50 +5,16 @@ describe("Create account", () => {
     await reloadApp();
     await element(by.id("ToCreateAccount")).tap();
   });
-
-  it("should display text Sign Up", async () => {
-    await expect(element(by.id("createAccountTitle"))).toBeVisible();
-  });
-
-  describe("Signup form", () => {
-    it("should display the signup form", async () => {
-      await expect(element(by.id("signupForm"))).toBeVisible();
-    });
-
-    it("should display a username label", async () => {
-      await expect(element(by.label("Username"))).toBeVisible();
-    });
-
-    it("should display a username input", async () => {
-      await expect(element(by.id("signupUsername"))).toBeVisible();
-    });
-
-    it("should display a email label", async () => {
-      await expect(element(by.label("Email"))).toBeVisible();
-    });
-
-    it("should display an email input", async () => {
-      await expect(element(by.id("signupEmail"))).toBeVisible();
-    });
-
-    it("should display a Password label", async () => {
-      await expect(element(by.label("Password"))).toBeVisible();
-    });
-
-    it("should display a password input", async () => {
-      await expect(element(by.id("signupPassword"))).toBeVisible();
-    });
-
-    it("should display a Confirm Password label", async () => {
-      await expect(element(by.label("Confirm Password"))).toBeVisible();
-    });
-
-    it("should display a confirmPassword input", async () => {
-      await expect(element(by.id("signupConfirmPassword"))).toBeVisible();
-    });
-
-    it("should display button", async () => {
-      await expect(element(by.id("signupFormButton"))).toBeVisible();
+  describe("interactions", () => {
+    describe("error path", () => {
+      describe("only entering username", () => {
+        it("should render an error message", async () => {
+          await element(by.id("signupUsername")).tap();
+          await element(by.id("signupUsername")).typeText("newuser");
+          await element(by.id("signupFormButton")).tap();
+          await expect(element(by.id("signupFormError"))).toBeVisible();
+        });
+      });
     });
   });
 });
