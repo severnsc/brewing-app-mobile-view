ac5308ae4aaf04383e24ac38db4063d375a0441a
Chris Severns <severnsc@gmail.com>
2019-02-16 20:55:42 -0500

    Mock react-apollo

---
+++ __mocks__/react-apollo.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const data = {
+  user: {
+    errors: []
+  }
+};
+
+export const graphql = jest.fn(() =>
+  jest.fn(
+    Component =>
+      class Container extends React.Component {
+        constructor(props) {
+          super(props);
+          this.state = {
+            data
+          };
+        }
+
+        mutate = jest.fn(({ variables }) => {
+          if (variables.username === "invalid") {
+            this.setState({
+              data: {
+                user: {
+                  ...this.state.data.user,
+                  errors: [
+                    ...this.state.data.user.errors,
+                    {
+                      message: "Username is invalid!",
+                      location: {
+                        node: "user",
+                        field: "username"
+                      }
+                    }
+                  ]
+                }
+              }
+            });
+          }
+        });
+
+        render() {
+          return <Component data={this.state.data} mutate={this.mutate} />;
+        }
+      }
+  )
+);
