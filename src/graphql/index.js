f35b3004b34196d7369682963b1f99e00b90081d
Chris Severns <severnsc@gmail.com>
2019-02-18 21:39:05 -0500

    Import/export resolvers

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,6 +1,7 @@
 import typeDefs from "./typeDefs";
 import defaults from "./defaults";
+import resolvers from "./resolvers";
 import { VALIDATE_USERNAME } from "./mutations";
 import { GET_USER } from "./queries";
 
-export { typeDefs, defaults, VALIDATE_USERNAME, GET_USER };
+export { typeDefs, defaults, resolvers, VALIDATE_USERNAME, GET_USER };
