05fc3c73544c508fc78654bc75cb8f33604faf20
Chris Severns <severnsc@gmail.com>
2019-02-25 19:51:01 -0500

    Update tests

--- src/containers/UsernameInput/usernameInput.test.js
+++ src/containers/UsernameInput/usernameInput.test.js
@@ -1,15 +1,8 @@
 import React from "react";
 import UsernameInput from ".";
 import { shallow } from "enzyme";
-import { mutate, compose } from "react-apollo";
 
-jest.mock("../../modules/validation");
 describe("UsernameInput", () => {
-  beforeEach(() => {
-    require("../../modules/validation");
-    jest.useFakeTimers();
-  });
-
   it("passes testID prop through to the base TextInput", () => {
     const usernameInput = shallow(<UsernameInput testID="test" />);
     const textInput = usernameInput.dive();
@@ -39,52 +32,120 @@ describe("UsernameInput", () => {
   describe("updating with valid value", () => {
     it("passes the value through to the TextInput", () => {
       const usernameInput = shallow(<UsernameInput />);
+      usernameInput.setProps({
+        data: {
+          user: {
+            id: "1",
+            username: "",
+            email: "",
+            errors: []
+          }
+        }
+      });
       const textInput = usernameInput.dive();
       expect(textInput.prop("value")).toBe("");
-      textInput.simulate("change", "valid");
-      usernameInput.update();
-      expect(textInput.prop("value")).toBe("valid");
-    });
-    it("leaves the TextInput isError as false", () => {
-      const usernameInput = shallow(<UsernameInput />);
-      const textInput = usernameInput.dive();
-      expect(textInput.prop("isError")).toBe(false);
-      textInput.simulate("change", "valid");
-      usernameInput.update();
-      expect(usernameInput.dive().prop("isError")).toBe(false);
-    });
-    it("leaves the TextInput errorText undefined", () => {
-      const usernameInput = shallow(<UsernameInput />);
-      const textInput = usernameInput.dive();
-      textInput.simulate("change", "valid");
-      expect(textInput.prop("errorText")).toBe(undefined);
-      usernameInput.update();
-      expect(usernameInput.dive().prop("errorText")).toBe(undefined);
+      usernameInput.setProps({
+        data: {
+          user: {
+            id: "1",
+            username: "valid",
+            email: "",
+            errors: []
+          }
+        }
+      });
+      expect(usernameInput.dive().prop("value")).toBe("valid");
     });
   });
 
   describe("updating with invalid value", () => {
     it("passes the value through to the TextInput", async () => {
       let usernameInput = shallow(<UsernameInput />);
+      usernameInput.setProps({
+        data: {
+          user: {
+            username: "",
+            errors: []
+          }
+        }
+      });
       const textInput = usernameInput.dive();
-      textInput.simulate("change", "invalid");
-      expect(textInput.prop("value")).toBe("invalid");
+      expect(textInput.prop("value")).toBe("");
+      usernameInput.setProps({
+        data: {
+          user: {
+            username: "invalid",
+            errors: []
+          }
+        }
+      });
+      expect(usernameInput.dive().prop("value")).toBe("invalid");
     });
     it("sets the TextInput isError to true", () => {
       const usernameInput = shallow(<UsernameInput />);
+      usernameInput.setProps({
+        data: {
+          user: {
+            username: "invalid",
+            errors: []
+          }
+        }
+      });
       const textInput = usernameInput.dive();
+      expect(textInput.prop("isError")).toBe(false);
       textInput.simulate("change", "invalid");
-      usernameInput.update();
+      usernameInput.setProps({
+        data: {
+          user: {
+            username: "invalid",
+            errors: [
+              {
+                __typename: "Error",
+                message: "Invalid username!",
+                location: {
+                  __typename: "Location",
+                  node: "user",
+                  field: "username"
+                }
+              }
+            ]
+          }
+        }
+      });
       expect(usernameInput.dive().prop("isError")).toBe(true);
     });
     it("sets the TextInput errorText to the error message", () => {
       const usernameInput = shallow(<UsernameInput />);
+      usernameInput.setProps({
+        data: {
+          user: {
+            username: "invalid",
+            errors: []
+          }
+        }
+      });
       const textInput = usernameInput.dive();
-      textInput.simulate("change", "invalid");
+      expect(textInput.prop("errorText")).toBe("");
+      usernameInput.setProps({
+        data: {
+          user: {
+            username: "invalid",
+            errors: [
+              {
+                __typename: "Error",
+                message: "Invalid username!",
+                location: {
+                  __typename: "Location",
+                  node: "user",
+                  field: "username"
+                }
+              }
+            ]
+          }
+        }
+      });
       usernameInput.update();
-      expect(usernameInput.dive().prop("errorText")).toBe(
-        "Username is invalid!"
-      );
+      expect(usernameInput.dive().prop("errorText")).toBe("Invalid username!");
     });
   });
 });
