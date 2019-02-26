cdc68f100f55abbd0b61336c2d9078bb081831dd
Chris Severns <severnsc@gmail.com>
2019-02-25 19:38:27 -0500

    Add exclude arguments for skipping return fields on user query

--- src/graphql/queries/user.js
+++ src/graphql/queries/user.js
@@ -1,12 +1,16 @@
 import gql from "graphql-tag";
 
 export const GET_USER = gql`
-  query {
+  query User(
+    $excludeUsername: Boolean
+    $excludeEmail: Boolean
+    $excludeErrors: Boolean
+  ) {
     user @client {
       id
-      username
-      email
-      errors {
+      username @skip(if: $excludeUsername)
+      email @skip(if: $excludeEmail)
+      errors @skip(if: $excludeErrors) {
         message
         location {
           node
