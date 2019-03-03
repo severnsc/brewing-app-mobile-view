596758c7d602fe350b67433c75bdf2fcbfc51d3f
Chris Severns <severnsc@gmail.com>
2019-03-03 09:49:21 -0500

    User create account screen in App router

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,6 @@
 import UsernameInput from "./UsernameInput";
 import EmailInput from "./EmailInput";
 import PasswordInput from "./PasswordInput";
+import CreateAccount from "./CreateAccount";
 
-export { UsernameInput, EmailInput, PasswordInput };
+export { UsernameInput, EmailInput, PasswordInput, CreateAccount };
