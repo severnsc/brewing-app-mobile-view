998c686d1e5847bcd19946f9cd07f98b96b1c23a
Chris Severns <severnsc@gmail.com>
2019-03-03 15:34:45 -0500

    Add new e2e tests

--- e2e/screens/CreateAccount/createAccount.spec.js
+++ e2e/screens/CreateAccount/createAccount.spec.js
@@ -8,15 +8,30 @@ describe("Create account", () => {
   describe("interactions", () => {
     describe("error path", () => {
       describe("only entering username", () => {
-        it("should render an error message for email, password & confirm password", async () => {
+        it("should render an error message for email & password", async () => {
           await element(by.id("signupUsername")).tap();
           await element(by.id("signupUsername")).typeText("newuser");
           await element(by.id("signupFormButton")).tap();
           await expect(element(by.id("emailInputError"))).toBeVisible();
           await expect(element(by.id("passwordInputError"))).toBeVisible();
-          await expect(
-            element(by.id("confirmPasswordInputError"))
-          ).toBeVisible();
+        });
+      });
+      describe("only entering email", () => {
+        it("should render an error message for username & password", async () => {
+          await element(by.id("signupEmail")).tap();
+          await element(by.id("signupEmail")).typeText("me@email.com");
+          await element(by.id("signupFormButton")).tap();
+          await expect(element(by.id("usernameInputError"))).toBeVisible();
+          await expect(element(by.id("passwordInputError"))).toBeVisible();
+        });
+      });
+      describe("only entering password", () => {
+        it("should render an error message for username & email", async () => {
+          await element(by.id("signupPassword")).tap();
+          await element(by.id("signupPassword")).typeText("password");
+          await element(by.id("signupFormButton")).tap();
+          await expect(element(by.id("usernameInputError"))).toBeVisible();
+          await expect(element(by.id("emailInputError"))).toBeVisible();
         });
       });
     });
