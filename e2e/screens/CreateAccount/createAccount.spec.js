6560d23b10efb3d6f9c50b89b44a9f068f447add
Chris Severns <severnsc@gmail.com>
2019-03-04 10:04:12 -0500

    Update e2e tests

--- e2e/screens/CreateAccount/createAccount.spec.js
+++ e2e/screens/CreateAccount/createAccount.spec.js
@@ -7,40 +7,39 @@ describe("Create account", () => {
   });
   describe("interactions", () => {
     describe("error path", () => {
-      describe("only entering username", () => {
-        it("should render an error message for email & password", async () => {
-          await element(by.id("signupUsername")).tap();
-          await element(by.id("signupUsername")).typeText("newuser");
-          await element(by.id("signupFormButton")).tap();
-          await expect(element(by.id("emailInputError"))).toBeVisible();
-          await expect(element(by.id("passwordInputError"))).toBeVisible();
-        });
-      });
-      describe("only entering email", () => {
-        it("should render an error message for username & password", async () => {
+      describe("when username is empty", () => {
+        it("should render an error message for username", async () => {
           await element(by.id("signupEmail")).tap();
-          await element(by.id("signupEmail")).typeText("me@email.com");
-          await element(by.id("signupFormButton")).tap();
-          await expect(element(by.id("usernameInputError"))).toBeVisible();
-          await expect(element(by.id("passwordInputError"))).toBeVisible();
-        });
-      });
-      describe("only entering password", () => {
-        it("should render an error message for username & email", async () => {
+          await element(by.id("signupEmail")).typeText("email@me.com");
           await element(by.id("signupPassword")).tap();
           await element(by.id("signupPassword")).typeText("password");
-          await element(by.id("signupFormButton")).tap();
-          await expect(element(by.id("usernameInputError"))).toBeVisible();
-          await expect(element(by.id("emailInputError"))).toBeVisible();
-        });
-      });
-      describe("only entering confirm password", () => {
-        it("should render error messages for username, email & password", async () => {
           await element(by.id("signupConfirmPassword")).tap();
           await element(by.id("signupConfirmPassword")).typeText("password");
           await element(by.id("signupFormButton")).tap();
           await expect(element(by.id("usernameInputError"))).toBeVisible();
+        });
+      });
+      describe("when email is empty", () => {
+        it("should render an error message for email", async () => {
+          await element(by.id("signupUsername")).tap();
+          await element(by.id("signupUsername")).typeText("newuser");
+          await element(by.id("signupPassword")).tap();
+          await element(by.id("signupPassword")).typeText("password");
+          await element(by.id("signupConfirmPassword")).tap();
+          await element(by.id("signupConfirmPassword")).typeText("password");
+          await element(by.id("signupFormButton")).tap();
           await expect(element(by.id("emailInputError"))).toBeVisible();
+        });
+      });
+      describe("when password is empty", () => {
+        it("should render an error message for password & confirmPassword", async () => {
+          await element(by.id("signupUsername")).tap();
+          await element(by.id("signupUsername")).typeText("newuser");
+          await element(by.id("signupEmail")).tap();
+          await element(by.id("signupEmail")).typeText("email@me.com");
+          await element(by.id("signupConfirmPassword")).tap();
+          await element(by.id("signupConfirmPassword")).typeText("password");
+          await element(by.id("signupFormButton")).tap();
           await expect(element(by.id("passwordInputError"))).toBeVisible();
           await expect(
             element(by.id("confirmPasswordInputError"))
