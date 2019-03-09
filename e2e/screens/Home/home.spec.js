3bd417fc0f7dc2fad695033cccf746488abbe4b3
Chris Severns <severnsc@gmail.com>
2019-03-09 17:44:21 -0500

    Remove unnecessary static specs

--- e2e/screens/Home/home.spec.js
+++ e2e/screens/Home/home.spec.js
@@ -4,18 +4,6 @@ describe("Home", () => {
   beforeEach(async () => {
     await reloadApp();
   });
-  it("should display Home text", async () => {
-    await expect(element(by.id("HomeText"))).toBeVisible();
-  });
-  it("should display image", async () => {
-    await expect(element(by.id("Hero"))).toBeVisible();
-  });
-  it("should have a button to create account", async () => {
-    await expect(element(by.id("ToCreateAccount"))).toBeVisible();
-  });
-  it("should have a button to login", async () => {
-    await expect(element(by.id("ToLogin"))).toBeVisible();
-  });
   describe("interactions", () => {
     describe("navigating to create account", () => {
       it("should display create account text after navigating", async () => {
