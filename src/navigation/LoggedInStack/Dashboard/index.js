3bf3a771a65332d18b50830e3971e21caeb6641e
Chris Severns <severnsc@gmail.com>
2019-03-10 13:15:38 -0400

    Add Dashboard route and LoggedInStack

---
+++ src/navigation/LoggedInStack/Dashboard/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Title from "../../../components";
+
+const dashboard = {
+  DASHBOARD: {
+    screen: () => null,
+    navigationOptions: {
+      title: "Home",
+      headerTitle: ({ allowFontScaling, style, children }) => (
+        <Title
+          value={children}
+          style={style}
+          allowFontScaling={allowFontScaling}
+          testID="createAccountTitle"
+          color="white"
+        />
+      )
+    }
+  }
+};
+
+export default dashboard;
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,10 +1,12 @@
 import { createStackNavigator } from "react-navigation";
 import NonLoggedInStack from "./NonLoggedInStack";
+import LoggedInStack from "./LoggedInStack";
 import { NOT_LOGGED_IN } from "../constants";
 
 export default createStackNavigator(
   {
-    NOT_LOGGED_IN: { screen: NonLoggedInStack }
+    NOT_LOGGED_IN: { screen: NonLoggedInStack },
+    LOGGED_IN: { screen: LoggedInStack }
   },
   {
     initialRoute: NOT_LOGGED_IN,
