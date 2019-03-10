c90ffc7e6b27283fa8ee5460c2e2f6c68bacd05f
Chris Severns <severnsc@gmail.com>
2019-03-10 13:25:20 -0400

    Make AppNav a Switch Nav

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,15 +1,14 @@
-import { createStackNavigator } from "react-navigation";
+import { createSwitchNavigator } from "react-navigation";
 import NonLoggedInStack from "./NonLoggedInStack";
 import LoggedInStack from "./LoggedInStack";
 import { NOT_LOGGED_IN } from "../constants";
 
-export default createStackNavigator(
+export default createSwitchNavigator(
   {
     NOT_LOGGED_IN: { screen: NonLoggedInStack },
     LOGGED_IN: { screen: LoggedInStack }
   },
   {
-    initialRoute: NOT_LOGGED_IN,
-    navigationOptions: { header: null, headerBackTitle: null }
+    initialRouteName: NOT_LOGGED_IN
   }
 );
