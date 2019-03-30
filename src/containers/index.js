4ab5d8fea840824fe6031c9254c0aa9a9de755cc
Chris Severns <severnsc@gmail.com>
2019-03-30 15:48:02 -0400

    Remove Password Input container

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,5 @@
-import PasswordInput from "./PasswordInput";
 import CreateAccount from "./CreateAccount";
 import Login from "./Login";
 import ForgotPassword from "./ForgotPassword";
 
-export { PasswordInput, CreateAccount, Login, ForgotPassword };
+export { CreateAccount, Login, ForgotPassword };
