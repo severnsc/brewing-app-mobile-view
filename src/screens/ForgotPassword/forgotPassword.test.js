ffe1b4427fe3803cc0df78c9fe78208f51edf6ac
Chris Severns <severnsc@gmail.com>
2019-05-16 10:48:28 -0400

    Refactor forgotPassword screen

--- src/screens/ForgotPassword/forgotPassword.test.js
+++ src/screens/ForgotPassword/forgotPassword.test.js
@@ -8,7 +8,11 @@ import renderer from "react-test-renderer";
 
 describe("Forgot Password screen", () => {
   const onSubmit = jest.fn();
-  const forgotPassword = shallow(<ForgotPassword onSubmit={onSubmit} />);
+  const onChange = jest.fn();
+  const value = "value";
+  const forgotPassword = shallow(
+    <ForgotPassword onSubmit={onSubmit} onChange={onChange} value={value} />
+  );
   it("matches snapshot", () => {
     const screen = renderer.create(<ForgotPassword onSubmit={onSubmit} />);
     const tree = screen.toJSON();
@@ -23,154 +27,69 @@ describe("Forgot Password screen", () => {
     expect(keyboardAvoid).toHaveLength(1);
     expect(keyboardAvoid.prop("style")).toEqual(styles.container);
   });
-  it("returns a Form with form styles", () => {
-    const form = forgotPassword.find("Form");
+  it("returns a View with form styles", () => {
+    const form = forgotPassword.findWhere(n => n.prop("style") === styles.form);
     expect(form).toHaveLength(1);
-    expect(form.prop("style")).toEqual(styles.form);
-  });
-  it("sets form initialValues to [{id: '1', value:''}, {id: '2', value: false}]", () => {
-    const form = forgotPassword.find("Form");
-    const initial = [{ id: "1", value: "" }, { id: "2", value: false }];
-    expect(form.prop("initialValues")).toEqual(initial);
-  });
-  it("sets form onSubmit to onSubmit prop", () => {
-    const form = forgotPassword.find("Form");
-    expect(form.prop("onSubmit")).toEqual(onSubmit);
   });
   describe("isError prop", () => {
     it("renders an INVALID_EMAIL error on TextInput when true", () => {
-      const onChange = jest.fn();
       const onSubmit = jest.fn();
       const forgotPassword = shallow(
         <ForgotPassword onSubmit={onSubmit} isError={true} />
       );
-      const form = forgotPassword.find("Form");
-      const formChildren = form
-        .props()
-        .children(
-          [{ id: "1", value: "" }, { id: "2", value: false }],
-          onChange,
-          onSubmit
-        );
-      const textInput = formChildren.props.children[0];
-      expect(textInput.props.errorText).toBe(INVALID_EMAIL);
+      const textInput = forgotPassword.find("TextInput");
+      expect(textInput.prop("errorText")).toBe(INVALID_EMAIL);
     });
     it("sets isError prop to true", () => {
-      const onChange = jest.fn();
       const onSubmit = jest.fn();
       const forgotPassword = shallow(
         <ForgotPassword onSubmit={onSubmit} isError={true} />
       );
-      const form = forgotPassword.find("Form");
-      const formChildren = form
-        .props()
-        .children(
-          [{ id: "1", value: "" }, { id: "2", value: false }],
-          onChange,
-          onSubmit
-        );
-      const textInput = formChildren.props.children[0];
-      expect(textInput.props.isError).toBe(true);
+      const textInput = forgotPassword.find("TextInput");
+      expect(textInput.prop("isError")).toBe(true);
+    });
+  });
+  describe("forgotPasswordError prop", () => {
+    it("renders an AlertIOS with forgotPasswordError message when truthy", () => {
+      const spy = jest.spyOn(AlertIOS, "alert");
+      shallow(<ForgotPassword forgotPasswordError={NETWORK_ERROR} />);
+      expect(spy).toHaveBeenCalledWith("Error!", NETWORK_ERROR);
+    });
+    it("does not render an AlertIOS when forgotPasswordError is falsy", () => {
+      const spy = jest.spyOn(AlertIOS, "alert");
+      spy.mockClear();
+      shallow(<ForgotPassword forgotPasswordError="" />);
+      expect(spy).not.toHaveBeenCalled();
     });
   });
   describe("Form children", () => {
     describe("TextInput", () => {
+      const emailInput = forgotPassword.find("TextInput");
       it("returns a TextInput with label Email", () => {
-        const onChange = jest.fn();
-        const onSubmit = jest.fn();
-        const emailInput = forgotPassword
-          .find("Form")
-          .props()
-          .children(
-            [{ id: "1", value: "" }, { id: "2", value: false }],
-            onChange,
-            onSubmit
-          ).props.children[0];
-        expect(emailInput.type.name).toBe("TextInput");
-        expect(emailInput.props.label).toBe("Email");
+        expect(emailInput.prop("label")).toBe("Email");
       });
       it("has input styles", () => {
-        const onChange = jest.fn();
-        const onSubmit = jest.fn();
-        const emailInput = forgotPassword
-          .find("Form")
-          .props()
-          .children(
-            [{ id: "1", value: "" }, { id: "2", value: false }],
-            onChange,
-            onSubmit
-          ).props.children[0];
-        expect(emailInput.props.style).toEqual(styles.input);
+        expect(emailInput.prop("style")).toEqual(styles.input);
       });
-      it("sets TextInput value equal to first value in values array", () => {
-        const onChange = jest.fn();
-        const onSubmit = jest.fn();
-        const textInput = forgotPassword
-          .find("Form")
-          .props()
-          .children(
-            [{ id: "1", value: "new value" }, { id: "2", value: false }],
-            onChange,
-            onSubmit
-          ).props.children[0];
-        expect(textInput.props.value).toBe("new value");
+      it("sets TextInput value equal to value prop", () => {
+        expect(emailInput.prop("value")).toBe(value);
       });
-      it("calls provided onChange function with 1 and new value onChange", () => {
-        const onChange = jest.fn();
-        const onSubmit = jest.fn();
-        const textInput = forgotPassword
-          .find("Form")
-          .props()
-          .children(
-            [{ id: "1", value: "new value" }, { id: "2", value: false }],
-            onChange,
-            onSubmit
-          ).props.children[0];
-        textInput.props.onChange("new value");
-        expect(onChange).toHaveBeenCalledWith("1", "new value");
+      it("calls provided onChange function with new value onChange", () => {
+        emailInput.props().onChange("new value");
+        expect(onChange).toHaveBeenCalledWith("new value");
       });
     });
     describe("Button", () => {
-      it("returns a Button with value Send Email", () => {
-        const onChange = jest.fn();
-        const onSubmit = jest.fn();
-        const button = forgotPassword
-          .find("Form")
-          .props()
-          .children(
-            [{ id: "1", value: "" }, { id: "2", value: false }],
-            onChange,
-            onSubmit
-          ).props.children[1];
-        expect(button.type.name).toBe("Button");
-        expect(button.props.value).toBe("Send Email");
+      const button = forgotPassword.find("Button");
+      it("has value Send Email", () => {
+        expect(button.prop("value")).toBe("Send Email");
       });
       it("has success and textColor white", () => {
-        const onChange = jest.fn();
-        const onSubmit = jest.fn();
-        const button = forgotPassword
-          .find("Form")
-          .props()
-          .children(
-            [{ id: "1", value: "" }, { id: "2", value: false }],
-            onChange,
-            onSubmit
-          ).props.children[1];
-        expect(button.props.success).toBe(true);
-        expect(button.props.textColor).toBe(white);
+        expect(button.prop("success")).toBe(true);
+        expect(button.prop("textColor")).toBe(white);
       });
       it("calls provided onSubmit onPress", () => {
-        const onChange = jest.fn();
-        const onSubmit = jest.fn(() => Promise.resolve());
-        const button = forgotPassword
-          .find("Form")
-          .props()
-          .children(
-            [{ id: "1", value: "" }, { id: "2", value: false }],
-            onChange,
-            onSubmit
-          ).props.children[1];
-        button.props.onPress({ id: "2", value: "email@example.com" });
+        button.props().onPress();
         expect(onSubmit).toHaveBeenCalled();
       });
     });
@@ -178,109 +97,11 @@ describe("Forgot Password screen", () => {
   describe("Submitting", () => {
     describe("when loading", () => {
       it("replaces the button with an ActivityIndicator", () => {
-        const onChange = jest.fn();
-        const onSubmit = jest.fn();
-        const activityIndicator = forgotPassword
-          .find("Form")
-          .props()
-          .children(
-            [{ id: "1", value: "" }, { id: "2", value: true }],
-            onChange,
-            onSubmit
-          ).props.children[1];
-        expect(activityIndicator.type.render.name).toBe("ActivityIndicator");
-      });
-    });
-    describe("onSubmit process", () => {
-      it("calls onChange with 2 and true", () => {
-        const onChange = jest.fn();
-        const onSubmit = jest.fn(() => Promise.resolve());
-        const button = forgotPassword
-          .find("Form")
-          .props()
-          .children(
-            [{ id: "1", value: "" }, { id: "2", value: false }],
-            onChange,
-            onSubmit
-          ).props.children[1];
-        const email = "email@example.com";
-        button.props.onPress({ id: "2", value: email });
-        expect(onChange).toHaveBeenCalledWith("2", true);
-      });
-      it("calls onSubmit with the provided email", () => {
-        const onChange = jest.fn();
-        const onSubmit = jest.fn(() => Promise.resolve());
-        const button = forgotPassword
-          .find("Form")
-          .props()
-          .children(
-            [{ id: "1", value: "" }, { id: "2", value: false }],
-            onChange,
-            onSubmit
-          ).props.children[1];
-        const email = "email@example.com";
-        button.props.onPress({ id: "2", value: email });
-        expect(onSubmit).toHaveBeenCalledWith(email);
-      });
-      describe("when onSubmit rejects", () => {
-        it("calls onChange with 2 and false", () => {
-          const onChange = jest.fn();
-          const onSubmit = jest.fn(() => Promise.reject());
-          const button = forgotPassword
-            .find("Form")
-            .props()
-            .children(
-              [{ id: "1", value: "" }, { id: "2", value: false }],
-              onChange,
-              onSubmit
-            ).props.children[1];
-          const email = "email@example.com";
-          button.props.onPress({ id: "2", value: email });
-          return Promise.resolve()
-            .then()
-            .then(() => {
-              expect(onChange).toHaveBeenCalledWith("2", false);
-            });
-        });
-        it("calls AlertIOS with a NETWORK_ERROR", () => {
-          const onChange = jest.fn();
-          const onSubmit = jest.fn(() => Promise.reject());
-          const button = forgotPassword
-            .find("Form")
-            .props()
-            .children(
-              [{ id: "1", value: "" }, { id: "2", value: false }],
-              onChange,
-              onSubmit
-            ).props.children[1];
-          const email = "email@example.com";
-          const spy = jest.spyOn(AlertIOS, "alert");
-          button.props.onPress({ id: "2", value: email });
-          return Promise.resolve()
-            .then()
-            .then(() => {
-              expect(spy).toHaveBeenCalledWith("Error!", NETWORK_ERROR);
-            });
-        });
-      });
-      describe("when onSubmit resolves", () => {
-        it("calls onChange with 2 and false", () => {
-          const onChange = jest.fn();
-          const onSubmit = jest.fn(() => Promise.resolve());
-          const button = forgotPassword
-            .find("Form")
-            .props()
-            .children(
-              [{ id: "1", value: "" }, { id: "2", value: false }],
-              onChange,
-              onSubmit
-            ).props.children[1];
-          const email = "email@example.com";
-          button.props.onPress({ id: "2", value: email });
-          return Promise.resolve().then(() => {
-            expect(onChange).toHaveBeenCalledWith("2", false);
-          });
-        });
+        const forgotPassword = shallow(<ForgotPassword loading={true} />);
+        const activityIndicator = forgotPassword.findWhere(
+          n => n.prop("animating") === true
+        );
+        expect(activityIndicator.length).toBe(1);
       });
     });
   });
