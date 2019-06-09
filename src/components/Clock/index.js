abd098152394f950ebb61d679180285985a534d4
Chris Severns <severnsc@gmail.com>
2019-06-09 13:41:36 -0400

    use time helper in Clock

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -2,20 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Text } from "..";
 import styles from "./styles";
-import moment from "moment";
+import { msToTimeString } from "../../helpers";
 
 const Clock = ({ ms, size }) => {
-	const time = moment.utc(ms).format("HH:mm:ss");
-	return <Text style={styles[size]} value={time} />;
+  const time = msToTimeString(ms);
+  return <Text style={styles[size]} value={time} />;
 };
 
 Clock.propTypes = {
-	ms: PropTypes.number.isRequired,
-	size: PropTypes.oneOf(["sm", "md", "lg"])
+  ms: PropTypes.number.isRequired,
+  size: PropTypes.oneOf(["sm", "md", "lg"])
 };
 
 Clock.defaultProps = {
-	size: "md"
+  size: "md"
 };
 
 export default Clock;
