11712eb7d459b4d6cbf11c831a777ffb6ff8c86a
Chris Severns <severnsc@gmail.com>
2019-06-09 00:11:59 -0400

    Export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import Button from "./Button";
+import Card from "./Card";
 import TextInput from "./TextInput";
 import NumberInput from "./NumberInput";
 import Text from "./Text";
 import Title from "./Title";
 import Subtitle from "./Subtitle";
 import Tab from "./Tab";
+import Tabs from "./Tabs";
 import Form from "./Form";
 import Icon from "./Icon";
 import GradientView from "./GradientView";
@@ -12,12 +14,14 @@ import ConfirmPasswordInput from "./ConfirmPasswordInput";
 
 export {
   Button,
+  Card,
   TextInput,
   NumberInput,
   Text,
   Title,
   Subtitle,
   Tab,
+  Tabs,
   Form,
   Icon,
   GradientView,
